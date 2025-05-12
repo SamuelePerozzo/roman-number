73cbbfb8af514d610ae075ebaa0fb5a1e3fbbd1d
SamuelePerozzo <samuele.perozzo@studenti.unipd.it>
2025-05-12 17:24:47 +0200

    Conversione e Test 10 Close #4

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -8,9 +8,8 @@ package it.unipd.mtss;
 public class IntegerToRoman {
     public static String convert(int number) {
 
-        String[] roman = {"M","CM","D","CD","C","XC","L",
-                        "XL","X","IX","V","IV","I"};
-        int[] arab = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] roman = {"X","IX","V","IV","I"};
+        int[] arab = {10, 9, 5, 4, 1};
 
         StringBuilder result = new StringBuilder();
 
