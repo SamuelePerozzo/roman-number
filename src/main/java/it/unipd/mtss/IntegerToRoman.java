30c0c1a1213a20665e31cb107c4c2fb41529fb69
SamuelePerozzo <samuele.perozzo@studenti.unipd.it>
2025-05-12 15:57:49 +0200

    Conversione e Test 3 Close #2

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -2,3 +2,18 @@
 // [Samuele] [Perozzo] [2110989]
 // [Lorenzo] [Grolla] [2105498]
 ////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+    public static String convert(int number) {
+        if (number < 1 || number > 3) {
+            throw new IllegalArgumentException("Number must be between 1 and 3");
+        }
+        String res = "";
+        for (int i = 0; i < number; i++) {
+            res += "I";
+        }
+        return res;
+    }
+}
--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,4 +1,18 @@
 //////////////////////////////////////////////////////////////////// 
-// [Samuele] [Perozzo] [2110989] 
-// [Lorenzo] [Grolla] [2105498] 
+// Samuele Perozzo 2110989
+// Lorenzo Grolla 2105498
 ////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class IntegerToRomanTest {
+    @Test
+    public void testConvert() {
+        assertEquals("I", IntegerToRoman.convert(1));
+        assertEquals("II", IntegerToRoman.convert(2));
+        assertEquals("III", IntegerToRoman.convert(3));
+    }
+}
